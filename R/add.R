5f46608ce221d0072d1da39fd0ec0bb277a5b54c
Xuri888 <601860806@qq.com>
2024-07-24 20:33:49 +0800

    Add files via upload

---
+++ R/add.R
@@ -0,0 +1,14 @@
+#' Return the sum of two numbers
+#'
+#' @param a number one
+#' @param b number two
+#'
+#' @return sum of the two numbers
+#' @export
+#'
+#' @examples
+#' library(newPackage)
+#' add(2, 5)
+add <- function(a, b) {
+  a + b
+}
---
+++ R/minus.R
@@ -0,0 +1,14 @@
+#' Return the minus of two numbers
+#'
+#' @param a Number one
+#' @param b Number two
+#'
+#' @return minus of the two numbers
+#' @export
+#'
+#' @examples
+#' library(newPackage)
+#' minus(1, 2)
+minus <- function(a, b) {
+  a - b
+}
